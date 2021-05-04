ad57b89f4f07397514184175161f8c8becee53fe
Touqeer Pathan <touqeer.pathan289@gmail.com>
2021-05-05 00:53:06 +0530

    email acknowledgement for contact us + abstract submission

--- conference/urls.py
+++ conference/urls.py
@@ -5,10 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # path('callback/', views.callback, name='callback'),
-    path('test_upload/', views.test_upload, name='test_upload'),
     path('', views.index, name='index'),
-    path('upload/', views.upload, name='upload'),
-    path('upload1/', views.upload1, name='upload'),
+    path('abstract-submission/', views.abstract_submission, name='abstract_submission'),
     path('about_conference/', views.about_conference, name='about_conference'),
     path('who-can-join/', views.who_should_join, name='who_should_join'),
     path('committee/', views.committee, name='committee'),
@@ -19,7 +17,7 @@ urlpatterns = [
     path('registration/early-bird', views.early_bird, name='early_bird'),
     path('registration/non-early', views.non_early, name='non_early'),
     path('call_for_papers/', views.call_for_papers, name='call_for_papers'),
-    path('abstract-submission/', views.abstract_submission, name='abstract_submission'),
+    path('abstract-submission-guidelines/', views.abstract_submission_guidelines, name='abstract_submission_guidelines'),
     path('publication-opportunities/', views.publication_opportunities, name='publication_opportunities'),
     path('evaluation_process/', views.evaluation_process, name='evaluation_process'),
     path('contact_us/', views.contact_us, name='contact_us'),
