a09c2462d78704c15b4002fcdd01600c9194bb52
Touqeer Pathan <touqeer.pathan289@gmail.com>
2021-06-12 02:11:36 +0530

    First commit on heroku

--- nitwConference/settings.py
+++ nitwConference/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'nitwConference.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gcimb',
-        'USER': 'postgres',
-        'PASSWORD': 'django123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
