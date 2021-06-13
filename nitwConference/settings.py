8b5ac4bded573235f8d6e04fba470fa2b92bfccf
Touqeer Pathan <touqeer.pathan289@gmail.com>
2021-06-13 13:00:55 +0530

    redirecting error fixed

--- nitwConference/settings.py
+++ nitwConference/settings.py
@@ -57,8 +57,8 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT = True
 
 ROOT_URLCONF = 'nitwConference.urls'
 
