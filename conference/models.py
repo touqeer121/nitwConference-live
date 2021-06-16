736d6b5da3c25512feff5c2bbdb6a95e0222e6ce
Touqeer Pathan <touqeer.pathan289@gmail.com>
2021-06-16 19:19:35 +0530

    added registrations model

--- conference/models.py
+++ conference/models.py
@@ -90,33 +90,33 @@ class Author_Type(models.Model) :
 		return self.author_type +" (" + str(self.id) + ")"
 
 
-# class Registration(models.Model) :
-# 	registration_id = models.CharField(max_length=20, primary_key=True)
-# 	# registration_type = models.ForeignKey(Registration_Type, verbose_name="Registration_Type", on_delete=models.CASCADE, blank=True, null=True)
-# 	# author_type = models.ForeignKey(Author_Type, verbose_name="Author_Type", on_delete=models.CASCADE, blank=True, null=True)
-# 	payment_method = models.CharField(max_length=200, blank=True, default='unknown')
-# 	transaction_id = models.CharField(max_length=100, blank=True, default='unknown')
-# 	abstract = models.ForeignKey(Abstract, verbose_name="Abstract", on_delete=models.CASCADE, blank=True, null=True)
-# 	prefix = models.CharField(max_length=20)
-# 	first_name = models.CharField(max_length=100)
-# 	last_name = models.CharField(max_length=100)
-# 	id_status = models.CharField(max_length=1, blank=True, default='2')
-# 	payment_status =  models.CharField(max_length=1, blank=True, default='2')
-# 	country = models.CharField(max_length=5000, blank=True, null=True, default='undefined')
-# 	state = models.CharField(max_length=500, blank=True, null=True, default='undefined')
-# 	institution = models.CharField(max_length=1000)
-# 	email = models.EmailField()
-# 	phone = models.CharField(max_length=20,
-# 							 validators=[RegexValidator(regex=r'[0-9]{10}', message='Invalid Mobile Number')],
-# 							 blank=True)
+class Registration(models.Model) :
+	registration_id = models.CharField(max_length=20, primary_key=True)
+	registration_type = models.ForeignKey(Registration_Type, verbose_name="Registration_Type", on_delete=models.CASCADE, blank=True, null=True)
+	author_type = models.ForeignKey(Author_Type, verbose_name="Author_Type", on_delete=models.CASCADE, blank=True, null=True)
+	payment_method = models.CharField(max_length=200, blank=True, default='unknown')
+	transaction_id = models.CharField(max_length=100, blank=True, default='unknown')
+	abstract = models.ForeignKey(Abstract, verbose_name="Abstract", on_delete=models.CASCADE, blank=True, null=True)
+	prefix = models.CharField(max_length=20)
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	id_status = models.CharField(max_length=1, blank=True, default='2')
+	payment_status =  models.CharField(max_length=1, blank=True, default='2')
+	country = models.CharField(max_length=5000, blank=True, null=True, default='undefined')
+	state = models.CharField(max_length=500, blank=True, null=True, default='undefined')
+	institution = models.CharField(max_length=1000)
+	email = models.EmailField()
+	phone = models.CharField(max_length=20,
+							 validators=[RegexValidator(regex=r'[0-9]{10}', message='Invalid Mobile Number')],
+							 blank=True)
 
-# 	id_proof = models.FileField(upload_to='maps', validators=[FileExtensionValidator(["png", "pdf", "jpeg", "jpg"])], 
-# 								storage=gd_storage, blank=True, default='undefined')
-# 	remark = models.CharField(max_length=5000, blank=True, default='')
-# 	registration_date = models.DateTimeField()
+	id_proof = models.FileField(upload_to='maps', validators=[FileExtensionValidator(["png", "pdf", "jpeg", "jpg"])], 
+								storage=gd_storage, blank=True, default='undefined')
+	remark = models.CharField(max_length=5000, blank=True, default='')
+	registration_date = models.DateTimeField()
 
-# 	def __str__(self):
-# 		return self.prefix +' '+ self.first_name + ' ' + self.last_name  + ' : ' + self.abstract.abs_id + "(" + str(self.registration_id) + ")"
+	def __str__(self):
+		return self.prefix +' '+ self.first_name + ' ' + self.last_name  + ' : ' + self.abstract.abs_id + "(" + str(self.registration_id) + ")"
 
 
 class File(models.Model):
@@ -150,8 +150,8 @@ class Registration_Count(models.Model):
 	def __unicode__(self):
 		return str(self.registration_count)
 
-# class Full_Paper_Count(models.Model):
-# 	full_paper_count = models.IntegerField(default=0)
+class Full_Paper_Count(models.Model):
+	full_paper_count = models.IntegerField(default=0)
 
-# 	def __unicode__(self):
-# 		return str(self.full_paper_count)
+	def __unicode__(self):
+		return str(self.full_paper_count)
