c001d5c24e75e0904c05d6c44895e897f214d448
Touqeer Pathan <touqeer.pathan289@gmail.com>
2021-06-30 14:25:35 +0530

    fixed value too long error 2

--- accounts/models.py
+++ accounts/models.py
@@ -9,11 +9,11 @@ gd_storage = GoogleDriveStorage()
 
 class TrackChairProfile(models.Model):
     user = models.OneToOneField(User,related_name='profile', on_delete=models.CASCADE)
-    phone = models.CharField(max_length=20,null=True, blank=True)
+    phone = models.CharField(max_length=200,null=True, blank=True)
     track = models.CharField(max_length=500,null=True)
     institution = models.CharField(max_length=500,null=True, blank=True)
     profile_pic = models.ImageField(upload_to='maps' ,null=True, blank=True)
-    prefix = models.CharField(max_length=20, blank=True)
+    prefix = models.CharField(max_length=200, blank=True)
     
     def __str__(self):
         return self.user.username
